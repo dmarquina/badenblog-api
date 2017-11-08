3a5d665f6643446fe95c7cf9c833452273489296
dmarquina <mrdiego0892@gmail.com>
2017-11-08 00:45:46 -0500

    searchBlank

--- src/main/java/com/badenblog/service/impl/PostServiceImpl.java
+++ src/main/java/com/badenblog/service/impl/PostServiceImpl.java
@@ -136,14 +136,18 @@ public class PostServiceImpl implements PostService {
 	@Override
 	@Transactional
 	public Page<PostFeedResponse> searchPosts(final Pageable pageable, final String searchField) {
-		long totalIdPosts = postDao.totalSearchPosts(searchField).longValue();
-		int offset = pageable.getOffset();
-		List<Integer> idPosts = postDao.searchPosts(offset,pageable.getPageSize(), searchField);
-		if (!idPosts.isEmpty()) {
-			List<PostFeedResult> result = postDao.findAllActives(idPosts);
-			return postFeedResultToPage(result, pageable, totalIdPosts);
-		} else {
-			return null;
+		if(!searchField.equals("")){
+			long totalIdPosts = postDao.totalSearchPosts(searchField).longValue();
+			int offset = pageable.getOffset();
+			List<Integer> idPosts = postDao.searchPosts(offset,pageable.getPageSize(), searchField);
+			if (!idPosts.isEmpty()) {
+				List<PostFeedResult> result = postDao.findAllActives(idPosts);
+				return postFeedResultToPage(result, pageable, totalIdPosts);
+			} else {
+				return null;
+			}
+		}else{
+			return findAllActives(pageable);
 		}
 	}
 
